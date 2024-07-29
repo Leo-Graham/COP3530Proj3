1196aebd4a148bc24eccd5000f3de7f72cbdeba5
Morki <61301652+Maykur@users.noreply.github.com>
2024-07-29 13:25:13 -0400

    Updated main + start on shell sort

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 #include <chrono>
 #include <fstream>
 #include <sstream>
+#include "ShellSort.h"
 using namespace std;
 using namespace chrono;
 
-string menuStartUp(){
+tuple<string,string,string> menuStartUp(){
     string option;
     string range;
+    string songRange;
 
     cout << "Welcome to our silly little program!" << endl;
     cout << endl;
@@ -21,6 +23,24 @@ string menuStartUp(){
 
     cin >> option;
 
+    cout << "What order would you like it in? (Type Number)" << endl;
+    cout << "   1) Ascending" << endl;
+    cout << "   2) Descending" << endl;
+
+    cin >> range;
+
+    cout << "How many songs would you like to be shown after sorting? (Type Number (1-352000))" << endl;
+
+    cin >> songRange;
+
+    if (stoi(songRange) > 352000 || stoi(songRange) < 1) {
+        cout << "Invalid input, terminating" << endl;
+        exit(0);
+    }
+
+    cout << endl;
+
+
     if(option == "1"){
         option = "Danceability";
     }
@@ -31,13 +51,18 @@ string menuStartUp(){
         option = "Etc.";
     }
 
-    return option;
+    if(range == "1"){
+        range = "Ascending";
+    }else if(range == "2"){
+        range = "Descending";
+    }
+
+    return make_tuple(option,range,songRange);
 }
 
-void fileRead(vector<tuple<string,string,string,string,string,string,string,string,string,string,string,string,
-        string,string,string,string,string,string,string,string,string,string,string,string>> &data){
+void fileRead(vector<tupInfo> &data){
     string info;
-    ifstream fileRead("../scrubbed dat.csv");
+    ifstream fileRead("../scrubbed dat (version 1).xlsb.csv");
     int cnt = 0;
     if (fileRead.is_open())
     {
@@ -46,33 +71,48 @@ void fileRead(vector<tuple<string,string,string,string,string,string,string,stri
             string id, name, album, album_id, artists, artist_ids, track_number, disc_number, explit, danceability,
                     energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms,
                     time_signature, year, release_date;
-            getline(inStream, id, ',');
-            getline(inStream, name, ',');
-            getline(inStream, album, ',');
-            getline(inStream, album_id, ',');
-            getline(inStream, artists, ',');
-            getline(inStream, artist_ids, ',');
-            getline(inStream, track_number, ',');
-            getline(inStream, disc_number, ',');
-            getline(inStream, explit, ',');
-            getline(inStream, danceability, ',');
-            getline(inStream, energy, ',');
-            getline(inStream, key, ',');
-            getline(inStream, loudness, ',');
-            getline(inStream, mode, ',');
-            getline(inStream, speechiness, ',');
-            getline(inStream, acousticness, ',');
-            getline(inStream, instrumentalness, ',');
-            getline(inStream, liveness, ',');
-            getline(inStream, valence, ',');
-            getline(inStream, tempo, ',');
-            getline(inStream, duration_ms, ',');
-            getline(inStream, time_signature, ',');
-            getline(inStream, year, ',');
-            getline(inStream, release_date);
+            getline(inStream, id, ','); // get<0>
+            getline(inStream, name, ','); // get<1>
+            getline(inStream, album, ','); // get<2>
+            getline(inStream, album_id, ','); // get<3>
+            getline(inStream, artists, ','); // get<4>
+            getline(inStream, artist_ids, ','); // get<5>
+            getline(inStream, track_number, ','); // get<6>
+            getline(inStream, disc_number, ','); // get<7>
+            getline(inStream, explit, ','); // get<8>
+            getline(inStream, danceability, ','); // get<9>
+            size_t find = danceability.find("E");
+            if(find != string::npos){
+                danceability = "0";
+            }
+            getline(inStream, energy, ','); // get<10>
+            find = energy.find("E");
+            if(find != string::npos){
+                energy = "0";
+            }
+            getline(inStream, key, ','); // get<11>
+            getline(inStream, loudness, ','); // get<12>
+            getline(inStream, mode, ','); // get<13>
+            getline(inStream, speechiness, ','); // get<14>
+            getline(inStream, acousticness, ','); // get<15>
+            find = acousticness.find("E");
+            if(find != string::npos){
+                acousticness = "0";
+            }
+            getline(inStream, instrumentalness, ','); // get<16>
+            find = instrumentalness.find("E");
+            if(find != string::npos){
+                instrumentalness = "0";
+            }
+            getline(inStream, liveness, ','); // get<17>
+            getline(inStream, valence, ','); // get<18>
+            getline(inStream, tempo, ','); // get<19>
+            getline(inStream, duration_ms, ','); // get<20>
+            getline(inStream, time_signature, ','); // get<21>
+            getline(inStream, year, ','); // get<22>
+            getline(inStream, release_date); // get<23>
 
-            tuple<string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,string,
-                    string,string,string,string,string,string> tup = make_tuple(id, name, album, album_id, artists, artist_ids, track_number, disc_number, explit, danceability,
+            tupInfo tup = make_tuple(id, name, album, album_id, artists, artist_ids, track_number, disc_number, explit, danceability,
                        energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms,
                        time_signature, year, release_date);
 
@@ -98,17 +138,51 @@ int main() {
     */
 
     // vector that has a tuple containing every single data variable for each data point in our set.
-    vector<tuple<string,string,string,string,string,string,string,string,string,string,string,string,
-            string,string,string,string,string,string,string,string,string,string,string,string>> dataInfo;
+    vector<tupInfo> dataInfo;
     // this reads the file and updates our vector to now hold all 300k+ data points
     fileRead(dataInfo);
 
-    string userInput;
-    // determines the variable we must sort by, through user input
-    userInput = menuStartUp();
+    string userInput,userSorting,userRange;
 
-    for(int i = 1; i < 11; i++){
-        cout << i << ". " << get<1>(dataInfo[i]) << endl;
+    // determines the variable we must sort by, through user input
+    tuple<string,string,string> userPairing = menuStartUp();
+    userInput = get<0>(userPairing);
+    userSorting = get<1>(userPairing);
+    userRange = get<2>(userPairing);
+
+    int choiceNum;
+
+    if(userInput == "Danceability"){
+        choiceNum = 9;
+    }else if(userInput == "Energy"){
+        choiceNum = 10;
+    }
+
+    ShellSorting(dataInfo, choiceNum);
+    if(choiceNum == 9) {
+        if (userSorting == "Ascending") {
+            for (int i = 1; i < stoi(userRange); i++) {
+                cout << i << ". " << get<1>(dataInfo[i]) << " " << get<9>(dataInfo[i]) << endl;
+            }
+        } else if (userSorting == "Descending") {
+            int counter = 1;
+            for (int i = dataInfo.size() - 5; i > dataInfo.size() - (stoi(userRange) + 5); i--) {
+                cout << counter << ". " << get<1>(dataInfo[i]) << " " << get<9>(dataInfo[i]) << endl;
+                counter += 1;
+            }
+        }
+    }else if(choiceNum == 10){
+        if (userSorting == "Ascending") {
+            for (int i = 1; i < stoi(userRange); i++) {
+                cout << i << ". " << get<1>(dataInfo[i]) << " " << get<10>(dataInfo[i]) << endl;
+            }
+        } else if (userSorting == "Descending") {
+            int counter = 1;
+            for (int i = dataInfo.size() - 5; i > dataInfo.size() - (stoi(userRange) + 5); i--) {
+                cout << counter << ". " << get<1>(dataInfo[i]) << " " << get<10>(dataInfo[i]) << endl;
+                counter += 1;
+            }
+        }
     }
 
     return 0;
